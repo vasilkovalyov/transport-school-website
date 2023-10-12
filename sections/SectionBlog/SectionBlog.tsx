52047e5f43cb6c34cce2b3ef80aa6d8eddb27d14
vasilkovalyov <vasilkovalyov@gmail.com>
2023-10-12 23:45:50 +0300

    created load more for section blog

--- sections/SectionBlog/SectionBlog.tsx
+++ sections/SectionBlog/SectionBlog.tsx
@@ -1,23 +1,60 @@
-import { BlogPost, Container } from '@/components/theme';
+'use client';
+import { useState, useEffect } from 'react';
+import { BlogPost, BlogPostProps, Container } from '@/components/theme';
 import { SectionBlogProps } from './SectionBlog.type';
+import { PAGE } from '@/axios/api-routes';
+import api from '@/axios/api';
 
-function SectionBlog({ heading, posts }: SectionBlogProps) {
+function SectionBlog({
+  heading,
+  posts,
+  next_page,
+  post_number,
+  current_page,
+}: SectionBlogProps) {
+  const [postsData, setPostsData] = useState<BlogPostProps[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(current_page || 2);
+  const [nextPage, setNextPage] = useState<number | null>(next_page || null);
+
+  useEffect(() => {
+    if (posts) {
+      setPostsData(posts);
+    }
+  }, [posts]);
+
+  async function loadPosts() {
+    const response = await api.get('posts', {
+      params: {
+        size: post_number,
+        page: currentPage,
+      },
+    });
+
+    setPostsData([...postsData, ...response.data.posts]);
+    setNextPage(response.data.next_page);
+    setCurrentPage(response.data.current_page);
+  }
+
   return (
     <section className="section-blog">
       <Container className="section-blog__container">
         <h2 className="section-blog__heading ta-c">{heading}</h2>
-        {posts && posts.length ? (
+        {postsData && postsData.length ? (
           <div className="blog-list">
-            {posts.map((post) => (
+            {postsData.map((post) => (
               <div key={post._id} className="blog-list__col">
-                <BlogPost {...post} />
+                <BlogPost {...post} slug={`${PAGE.BLOG}/${post._id}`} />
               </div>
             ))}
           </div>
         ) : null}
-        <div className="section-blog__action ta-c">
-          <button className="load-more">Загрузить больше</button>
-        </div>
+        {nextPage ? (
+          <div className="section-blog__action ta-c">
+            <button className="load-more" onClick={loadPosts}>
+              Загрузить больше
+            </button>
+          </div>
+        ) : null}
       </Container>
     </section>
   );
--- sections/SectionBlog/SectionBlog.type.ts
+++ sections/SectionBlog/SectionBlog.type.ts
@@ -4,6 +4,9 @@ import { IBlockBaseInfo } from '../types';
 export type SectionBlogProps = {
   heading: string;
   posts?: BlogPostProps[];
+  next_page?: number | null;
+  post_number?: number | null;
+  current_page: number;
 };
 
 export type BlockBlogFullDataType = SectionBlogProps & IBlockBaseInfo;
